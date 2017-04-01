c3c5ea5d40aaf6f37187c9c075cce836aacb2e37
Gabriel Esteban <gabriel.esteban.gullon@gmail.com>
2017-04-01 23:33:12 +0100

    Get balances from poloniex api

--- api/routes/userroutes.js
+++ api/routes/userroutes.js
@@ -2,17 +2,19 @@ const usercontroller = require('../controllers/usercontroller');
 
 module.exports = function(app){
 
+    // just for testing
     app.route('/user')
             .get(usercontroller.get_static_user);
 
     app.route('/user/:userId/movement')
             .get(usercontroller.list_user_movements);
 
-    app.route('/user/:userId/movement/create')
-            .post(usercontroller.create_movement);
-
     app.route('/user/:userId/balance')
             .get(usercontroller.list_user_balance);
+
+    // manual update methods
+    app.route('/user/:userId/movement/create')
+            .post(usercontroller.create_movement);
 
     app.route('/user/:userId/balance/add')
             .post(usercontroller.add_balance);
--- init_test_data.js
+++ init_test_data.js
@@ -2,8 +2,9 @@
 const mongoose = require('mongoose');
 require('./models/user');
 const User = mongoose.model('User');
-cosnt dotenv = require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
+const Poloniex = require('poloniex-api-node');
 
 // connecting to mongo
 mongoose.promise = global.Promise;
@@ -17,10 +18,7 @@ User.remove({}, (err) => {
 let user = new User({
     poloniex_key: process.env.POLONIEX_KEY,
     poloniex_secret: process.env.POLONIEX_SECRET,
-    balances: [
-        {'currency': 'ETH', 'amount': 8.64841},
-        {'currency': 'BTC', 'amount': 0.54896}
-    ],
+    balances: [],
     movements: []
 });
 user.save((err, user) => {
@@ -28,4 +26,24 @@ user.save((err, user) => {
     else console.log('Created user');
 });
 
-mongoose.disconnect();
+User.update({_id: user._id}, {$inc: {nonce: 1}}, (err, raw) => {
+    console.log('Updated nonce');
+});
+
+let poloniex = new Poloniex(user.poloniex_key, user.poloniex_secret);
+poloniex.returnBalances((err, balances) => {
+    let new_balances = [];
+    for(let balance in balances){
+        if (balances.hasOwnProperty(balance)) {
+            if(balances[balance] > 0) new_balances.push({currency: balance, amount: balances[balance]});
+        }
+    }
+    User.update({_id: user._id},
+                {$set: {balances: new_balances}}, (err, raw) => {
+                    if(err) console.log(err);
+                    else {
+                        console.log('Added balances');
+                        mongoose.disconnect();
+                    }
+                });
+});
